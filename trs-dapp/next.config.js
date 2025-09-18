278cb00ac9f12ce8a10be5b32f2069f579f4b6ad
Reza Azizian <reza@example.com>
2025-09-18 16:34:00 -0400

    chore(dapp): configure static export for Vercel

--- trs-dapp/next.config.js
+++ trs-dapp/next.config.js
@@ -1,6 +1,8 @@
-﻿/** @type {import(''next'').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  output: 'export',
+  images: { unoptimized: true }
 };
 module.exports = nextConfig;
